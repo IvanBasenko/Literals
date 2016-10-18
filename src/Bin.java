62089df6227f647332298a900717f870153472d8
Ivan Basenko <primeivan28@gmail.com>
2016-10-19 02:20:49 +0300

    zbs

--- src/Bin.java
+++ src/Bin.java
@@ -28,16 +28,67 @@ public class Bin {
                 System.out.println("Не четное: " + i);
         }
 
-        int i, b;
-        for (i = 1; i <= 40; i++) {
-            b = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) b++;
+        {
+            int i, b;
+            for (i = 1; i <= 40; i++) {
+                b = 0;
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) b++;
+                }
+                if (b <= 2)
+                    System.out.print(i + " ");
             }
-            if (b <= 2)
-                System.out.println(i);
+            System.out.println();
+        }
+        /*int count = 11;
+        int smh = 0;
+        for (int i = 0; i < count; i++) {
+            if (i < count / 2) {
+                smh = count / 2 + i;
+            } else {
+                smh = i - count / 2;
+            }
+            for (int k = 0; k < count; k++) {
+                if (k == smh || k == count - smh - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }*/
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j <=i; j++) {
+            if (j<7){
+                System.out.print("*");
+            }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 5; i>=0; i--) {
+            for (int j = 5; j >=0; j--) {
+            if (j<=i){
+                System.out.print("+");
+            }else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 5; i>=0; i--) {
+            for (int j = 5; j >=0; j--) {
+            if (j<=5-i){
+                System.out.print("+");
+            }else
+                System.out.print(" ");
+            }
+            System.out.println();
         }
     }
 }
+
 
 
